0aea3925d4071b4c4662b5468b8ff3bf09447b6c
IKE1213 <ikedakoukun1213@gmail.com>
2025-04-22 14:57:51 +0900

    test commit

--- src/main/java/servlet/UranaiServlet.java
+++ src/main/java/servlet/UranaiServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,9 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UranaiServlet
- */
+
 @WebServlet("/UranaiServlet")
 public class UranaiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -19,7 +18,16 @@ public class UranaiServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.print("…");
+		out.print("</html>");
+	response.setContentType("text/html; charset=UTF-8"); // HTMLをUTF-8で返す設定
+	response.getWriter().println("<html><body>");
+	response.getWriter().println("<h1>こんにちは、サーブレット！</h1>");
+	response.getWriter().println("<h1>こんにちは、サーブレット！</h1>");
+	response.getWriter().println("</body></html>");
 	}
 
 }
